67eeefe538c6df53f148cd6d8838f68eb9e181a1
OfficialPapu <papuyadavofficial386@gmail.com>
2025-01-30 04:26:23 +0545

    project completed

--- client/Components/Navbar.js
+++ client/Components/Navbar.js
@@ -111,7 +111,7 @@ export function Navbar() {
               <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                 <Avatar className="h-8 w-8">
                   <AvatarImage src="/avatars/01.png" alt="@username" />
-                  <AvatarFallback>U</AvatarFallback>
+                  <AvatarFallback>A</AvatarFallback>
                 </Avatar>
               </Button>
             </DropdownMenuTrigger>
